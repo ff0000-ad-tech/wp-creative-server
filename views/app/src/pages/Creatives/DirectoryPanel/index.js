7b2b1a3ca14f478070409b2143dc34eb5352baf3
Nico Hsieh <nico.hsieh@ff0000.com>
2017-12-04 10:44:49 -0800

    change folder names to lowercase

---
+++ views/app/src/pages/Creatives/DirectoryPanel/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+class DirectoryPanel extends Component {
+
+  render() {
+    return (
+      <div className='directory-panel'>
+      	<div className='title'>~</div>
+      </div>
+    )
+  }
+}
+ 
+export default DirectoryPanel
