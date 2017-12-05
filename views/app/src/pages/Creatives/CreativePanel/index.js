37618ef94ea5fa1f79f740ee36ce15482e18b730
Nico Hsieh <nico.hsieh@ff0000.com>
2017-12-04 23:45:27 -0800

    add single creative component

--- views/app/src/pages/Creatives/CreativePanel/index.js
+++ views/app/src/pages/Creatives/CreativePanel/index.js
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react'
+import SingleCreative from '../SingleCreative'
+
 import './style.scss'
 
 class CreativePanel extends PureComponent {
 	render() {
+		// temp
+		const builds = [{}, {}]
 		return (
 			<div className="creative-panel" style={this.props.style}>
 				<div className="title">wp-creative-server</div>
+				<ul className="header-row">
+					<li className="build-col col">Builds</li>
+					<li className="compile-col col">Compiling</li>
+					<li className="deploy-col col">Deployed</li>
+				</ul>
+				<ul>
+					{builds.map((item, index) => {
+						return <SingleCreative data={item} key={index} />
+					})}
+				</ul>
 			</div>
 		)
 	}
