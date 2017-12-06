4e3e1e385cddb12493800ad3bfd71ef3325668ef
Nico Hsieh <nico.hsieh@ff0000.com>
2017-12-05 23:16:35 -0800

    fix incorrect merge

--- views/app/src/pages/Creatives/BrowsePanel/index.js
+++ views/app/src/pages/Creatives/BrowsePanel/index.js
@@ -6,66 +6,81 @@ const log = debug('wp-cs:BrowsePanel')
 import './style.scss'
 
 class BrowsePanel extends PureComponent {
-	constructor() {
-		super()
-		// dissect current location for history management
-		this.componentPaths = this.getComponentPaths(document.location)
+  constructor() {
+    super()
 
-		// prevent iframe onload when popping
-		// this.popping = false
-		// // listen for pop-state
-		// window.onpopstate = (e) => {
-		// 	log('popped to:', document.location)
-		// 	const paths = this.getComponentPaths(document.location)
-		// 	this.popping = true
-		// 	this.refs['browse-iframe'].src = `${this.componentPaths.origin + paths.browseRoute}`
-		// }
-	}
+    this.state = {
+      deactivated: false
+    }
+    // dissect current location for history management
+    this.componentPaths = this.getComponentPaths(document.location)
 
-	render() {
-		return (
-			<div className="browse-panel">
-				<iframe ref="browse-iframe" 
-					src={`${this.componentPaths.origin + this.componentPaths.browseRoute}`} 
-					onLoad={this.handleIframeLoad.bind(this)}
-				></iframe>
-			</div>
-		)
-	}
+    // prevent iframe onload when popping
+    // this.popping = false
+    // // listen for pop-state
+    // window.onpopstate = (e) => {
+    // 	log('popped to:', document.location)
+    // 	const paths = this.getComponentPaths(document.location)
+    // 	this.popping = true
+    // 	this.refs['browse-iframe'].src = `${this.componentPaths.origin + paths.browseRoute}`
+    // }
+  }
 
+  onDragChanged = state => {
+    this.setState({
+      deactivated: state
+    })
+  }
 
-	getComponentPaths(location) {
-		const origin = location.origin
+  getComponentPaths(location) {
+    const origin = location.origin
 
-		// app route like: "app", "app/", "app#/"
-		const appRoute = location.href.slice(origin.length).match(/^\/[^\/]*/)[0]
+    // app route like: "app", "app/", "app#/"
+    const appRoute = location.href.slice(origin.length).match(/^\/[^\/]*/)[0]
 
-		// browse route like: "build/300x250"
-		const browseRoute = location.href.slice((origin + appRoute).length)
-		
-		const componentPaths = {
-			origin,
-			appRoute,
-			browseRoute
-		}
-		return componentPaths
-	}
+    // browse route like: "build/300x250"
+    const browseRoute = location.href.slice((origin + appRoute).length)
 
+    const componentPaths = {
+      origin,
+      appRoute,
+      browseRoute
+    }
+    return componentPaths
+  }
 
+  handleIframeLoad(e) {
+    // let the next reload alter the history
+    // if (this.popping) {
+    // 	log('<<<<< popping complete')
+    // 	log(window.history)
+    // 	this.popping = false
+    // 	return
+    // }
+    const paths = this.getComponentPaths(e.target.contentWindow.location)
+    const browseRoute = e.target.contentWindow.location.href.slice(
+      paths.origin.length
+    )
+    window.history.replaceState(
+      {},
+      '',
+      this.componentPaths.appRoute + browseRoute
+    )
+  }
 
-
-	handleIframeLoad(e) {
-		// let the next reload alter the history
-		// if (this.popping) {
-		// 	log('<<<<< popping complete')
-		// 	log(window.history)
-		// 	this.popping = false
-		// 	return
-		// }
-		const paths = this.getComponentPaths(e.target.contentWindow.location)
-		const browseRoute = e.target.contentWindow.location.href.slice(paths.origin.length)
-		window.history.replaceState({}, '', this.componentPaths.appRoute + browseRoute);
-	}
+  render() {
+    const deactivatedClass = this.state.deactivated ? 'deactivated' : ''
+    return (
+      <div className={`browse-panel ${deactivatedClass}`}>
+        <iframe
+          ref="browse-iframe"
+          src={`${this.componentPaths.origin +
+            this.componentPaths.browseRoute}`}
+          onLoad={this.handleIframeLoad.bind(this)}
+        />
+      </div>
+    )
+  }
 }
 
 export default BrowsePanel
