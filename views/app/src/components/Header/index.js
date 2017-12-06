7ea8612a8db345068052f80cc7bf9a6aa83424c1
Greg Connell <greg.connell@ff0000.com>
2017-12-06 00:27:38 -0800

    fixing the header component that got kludged by `Components` change to `components`

---
+++ views/app/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React, {PureComponent} from 'react'
+import './style.scss'
+import logo from './images/icon_32.png'
+
+class Header extends PureComponent {
+	render() {
+		return (
+			<header className="header">
+				<div className="logo-container clear-after">
+					<a href="https://github.com/ff0000-ad-tech" target="_blank">
+						<img src={logo} className="left" />
+					</a>
+					<h1 className="left">Creative Server</h1>
+				</div>
+			</header>
+		)
+	}
+}
+
+export default Header
