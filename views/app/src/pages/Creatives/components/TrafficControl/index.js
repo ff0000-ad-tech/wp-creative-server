037cfbfe5404642078cbaff2c97851bd127a7e94
Greg Connell <greg.connell@ff0000.com>
2018-01-17 15:07:09 -0800

    traffic control bar progress

--- views/app/src/pages/Creatives/components/TrafficControl/index.js
+++ views/app/src/pages/Creatives/components/TrafficControl/index.js
@@ -5,6 +5,7 @@ import debug from 'debug'
 const log = debug('wp-cs:app:TrafficControl')
 
 import './style.scss'
+import settingsIcon from './images/settings-icon.svg'
 
 class TrafficControl extends PureComponent {
 	render() {
@@ -13,17 +14,31 @@ class TrafficControl extends PureComponent {
 			<div className="traffic-control-options">
 				<li className="build-col col"></li>
 				<li className="debug-col col"></li>
-				<li className="traffic-col col" />
-				<li className="last-traffic-col col">
-					<div class="clear-after">
-						<div class="option-checkbox right">
-							<input type="checkbox" />
+				<li className="settings-col col">
+					<div className="clear-after">
+						<div className="right clear-after">
+							<div className="option-checkbox right">
+								<input type="checkbox" />
+							</div>
+							<div className="option-button right">
+								<input type="button" value="DEPLOY" />
+							</div>
 						</div>
-						<div class="option-button right">
-							<input type="button" value="DEPLOY" />
-						</div>
-						<div class="option-button right">
-							<input type="button" value="Settings" />
+
+						<div className="left clear-after">
+							<div className="option-button left" style={{ paddingLeft:'0px' }}>
+								<input type="button" value="+" />
+							</div>
+							<div className="option-button left">
+								<select>
+									<option>Default</option>
+								</select>
+							</div>
+							<div className="option-button left">
+								<div style={{ paddingTop: '2px' }}>
+									<img src={settingsIcon} width="16" height="16" />
+								</div>
+							</div>
 						</div>
 					</div>
 				</li>
