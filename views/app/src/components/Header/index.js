ea168f8ba320a9a0eb8b6355cfd996082acef949
Greg Connell <greg.connell@ff0000.com>
2018-03-13 12:57:59 -0700

    adds version to header

--- views/app/src/components/Header/index.js
+++ views/app/src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React, { PureComponent } from 'react'
 import './style.scss'
 import logo from './images/icon_32.png'
+import appPackage from 'Root/package.json'
+
+import debug from 'debug'
+const log = debug('wp-cs:app:Header')
 
 class Header extends PureComponent {
+	componentDidMount() {
+		log(appPackage)
+	}
 	refreshApp = () => {
 		location.href = '/app'
 	}
@@ -10,10 +17,13 @@ class Header extends PureComponent {
 		return (
 			<header onClick={this.refreshApp}>
 				<div className="logo-container clear-after">
-					<a href="https://github.com/ff0000-ad-tech" target="_blank">
-						<img src={logo} className="left" />
-					</a>
-					<h1 className="left">Creative Server</h1>
+					<div className="left">
+						<a href="https://github.com/ff0000-ad-tech" target="_blank">
+							<img src={logo} className="left" />
+						</a>
+					</div>
+					<div className="app-name left">Creative Server</div>
+					<div className="version left">v{appPackage.version}</div>
 				</div>
 			</header>
 		)
