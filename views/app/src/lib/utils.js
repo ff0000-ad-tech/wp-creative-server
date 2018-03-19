2476cce6919a8c7d48f0ebe2f50923187ea200e0
Greg Connell <greg.connell@ff0000.com>
2018-03-19 11:39:38 -0700

    organizes origin/host refs

--- views/app/src/lib/utils.js
+++ views/app/src/lib/utils.js
@@ -21,12 +21,11 @@ export function xhr(url, callback) {
 export function getPluginRequest(plugin, route, args) {
 	const p = getPluginRoute(route)
 	const query = p.query ? `?${p.query}&` : `?`
-	let qs = ''
+	let qs = `api=${encodeURIComponent(window.location.host)}`
 	if (args) {
 		Object.keys(args).forEach(arg => {
 			qs += `&${arg}=${encodeURIComponent(JSON.stringify(args[arg]))}`
 		})
-		qs = qs.slice(1)
 	}
 	return `/${plugin}${p.route}/${query}${qs}`
 }
